b8b571e7d9c8cba8de00ae04e18b2807dacb39ac
francesshei <frances.j.shei@gmail.com>
2021-10-05 09:41:52 +0200

    Updated grid points placement

---
+++ test/test_basics.py
@@ -0,0 +1,20 @@
+import unittest
+import radialdf
+import numpy as np
+import plotly.graph_objects as go
+
+class TestRDF(unittest.TestCase):
+    def test_box(self):
+        # Generate 10000 random particles with 3 coordinates between 0 and 100
+        side = 10
+        resolution = 0.2
+        particles = np.random.rand(1000, 3) * side
+        # Define a volume from 0 to 100 on 3 axes
+        box = radialdf.Box(side=side)
+        g = radialdf.volume_rdf(box, particles, 4, resolution)
+        ##self.assertAlmostEqual(4, len(g), "This should have been 4")
+        print("RESULT:", g)
+        go.Figure(go.Scatter(x=[i * resolution for i in range(int(side // resolution))], y=g)).show()
+    
+    def test_lala(self):
+        pass
